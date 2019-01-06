f2f02b15734790a991264e53636f3333305dd978
Chewei <a24553120@gmail,com>
2019-01-06 15:09:45 +0800

    Update TaskApp.c

--- Project/RVMDK（uv5）/TaskApp.c
+++ Project/RVMDK（uv5）/TaskApp.c
@@ -23,6 +23,7 @@ void  AppTaskTouchScan  (void )
 }
 	
 
+/*show the system time of game start to end*/
 void  AppTaskTmr_ ( void * p_arg )
 {
 
@@ -48,22 +49,20 @@ void  AppTaskTmr_ ( void * p_arg )
 
 
 /*
-*********************************************************************************************************
-*                                          TMR TASK
-*********************************************************************************************************
+/*show the system time of game stop/
+
 */
 void TmrCallback (OS_TMR *p_tmr, void *p_arg) //软件定时器MyTmr的回调函数
 {
 	CPU_SR_ALLOC();      //使用到临界段（在关/开中断时）时必需该宏，该宏声明和定义一个局部变
 											 //量，用于保存关中断前的 CPU 状态寄存器 SR（临界段关中断只需保存SR）
 											 //，开中断时将该值还原。  
-  //printf ( "%s", ( char * ) p_arg );
 
-stoptim_cnt++;	
+    stoptim_cnt++;	
 
 	OS_CRITICAL_ENTER();                 //进入临界段，不希望下面串口打印遭到中断
 	
-printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
+    printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
 	
 	OS_CRITICAL_EXIT();                               
 
@@ -79,6 +78,7 @@ printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
 /*
 *********************************************************************************************************
 *                                          KEY1 TASK
+press key1 to stop the game
 *********************************************************************************************************
 */
   void  AppTaskKey1 ( void * p_arg )
@@ -95,20 +95,13 @@ printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
 	while (DEF_TRUE) {                                                         //任务体
 		if( Key_Scan ( macKEY1_GPIO_PORT, macKEY1_GPIO_PIN, 1, & ucKey1Press ) ) //如果KEY1被单击
 		{
+#if 0
 			ctr = OSSemPend ((OS_SEM   *)&SemOfKey,               //等待该信号量 SemOfKey
 								       (OS_TICK   )0,                       //下面选择不等待，该参无效
 								       (OS_OPT    )OS_OPT_PEND_BLOCKING,//如果没信号量可用不等待
 								       (CPU_TS   *)0,                       //不获取时间戳
 								       (OS_ERR   *)&err);                   //返回错误类型
-			
-			OS_CRITICAL_ENTER();                                  //进入临界段
-			
-			if ( err == OS_ERR_NONE )                      
-				printf ( "\r\nKEY1被单击：成功申请到停车位，剩下%d个停车位。", ctr );
-			else if ( err == OS_ERR_PEND_WOULD_BLOCK )
-				printf ( "\r\nkey1被单击：不好意思，现在停车场已满，请等待！" );
-			
-			OS_CRITICAL_EXIT(); 
+#endif
             OSTaskSuspend ( &AppTaskGUIBaseTCB, & err );
             stoptim_cnt=0;
             OSTmrStart ((OS_TMR   *)&my_tmr, //软件定时器对象
@@ -129,6 +122,7 @@ printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
 /*
 *********************************************************************************************************
 *                                          KEY2 TASK
+press key2 to resume the game
 *********************************************************************************************************
 */
   void  AppTaskKey2 ( void * p_arg )
@@ -147,15 +141,11 @@ printf("stoptime=%d:%d",stoptim_cnt/60,stoptim_cnt%60);
 	while (DEF_TRUE) {                                                         //任务体
 		if( Key_Scan ( macKEY2_GPIO_PORT, macKEY2_GPIO_PIN, 1, & ucKey2Press ) ) //如果KEY2被单击
 		{
+#if 0
 		  ctr = OSSemPost((OS_SEM  *)&SemOfKey,                                  //发布SemOfKey
 							        (OS_OPT   )OS_OPT_POST_ALL,                            //发布给所有等待任务
 							        (OS_ERR  *)&err);                                      //返回错误类型
-
-			OS_CRITICAL_ENTER();                                                   //进入临界段
-			
-			printf ( "\r\nKEY2被单击：释放1个停车位，剩下%d个停车位。", ctr );
-			
-			OS_CRITICAL_EXIT();
+#endif
 			OSTmrStop((OS_TMR   *)&my_tmr,OS_OPT_TMR_NONE,0,(OS_ERR   *)err);
             OSTaskResume ( &AppTaskGUIBaseTCB, & err );
 
